c18178d043b6b5b4c4f48343cfff48959a8e97c8
Eddie Wang <eddieshengyuwang@gmail.com>
2018-09-20 15:22:47 +0200

    add some code to Model

--- lastname1-lastname2-in/src/RabbitsGrassSimulationModel.java
+++ lastname1-lastname2-in/src/RabbitsGrassSimulationModel.java
@@ -1,4 +1,5 @@
 import uchicago.src.sim.engine.Schedule;
+import uchicago.src.sim.engine.SimInit;
 import uchicago.src.sim.engine.SimModelImpl;
 
 /**
@@ -12,21 +13,37 @@ import uchicago.src.sim.engine.SimModelImpl;
 
 
 public class RabbitsGrassSimulationModel extends SimModelImpl {		
-
+		private Schedule schedule;
+		
+		private int numAgents;
+		private int worldXSize;
+		private int worldYSize;
+		
 		public static void main(String[] args) {
 			
 			System.out.println("Rabbit skeleton");
-			
+			SimInit init = new SimInit();
+			RabbitsGrassSimulationModel model = new RabbitsGrassSimulationModel();
+			init.loadModel(model, "", false);
 		}
 
 		public void begin() {
 			// TODO Auto-generated method stub
+			buildModel();
+			buildSchedule();
+			buildDisplay();
+		}
+		
+		public void buildModel() {
 			
 		}
-
-		public String[] getInitParam() {
-			// TODO Auto-generated method stub
-			return null;
+		
+		public void buildSchedule() {
+			
+		}
+		
+		public void buildDisplay() {
+			
 		}
 
 		public String getName() {
@@ -36,11 +53,41 @@ public class RabbitsGrassSimulationModel extends SimModelImpl {
 
 		public Schedule getSchedule() {
 			// TODO Auto-generated method stub
-			return null;
+			return schedule;
 		}
 
 		public void setup() {
 			// TODO Auto-generated method stub
 			
 		}
+		
+		public String[] getInitParam() {
+			String[] initParams = {"NumAgents", "WorldXSize", "WorldYSize"};
+			return initParams;
+		}
+		
+		public int getNumAgents(){
+			return numAgents;
+		}
+
+		public void setNumAgents(int na){
+			numAgents = na;
+		}
+		
+		public int getWorldXSize(){
+			return numAgents;
+		}
+
+		public void setWordXSize(int wxs){
+			worldXSize = wxs;
+		}
+		
+		public int getWorldYSize(){
+			return worldYSize;
+		}
+
+		public void setWorldYSize(int wys){
+			worldYSize = wys;
+		}
 }
+
